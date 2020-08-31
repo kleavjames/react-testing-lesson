21370904a59d1c3545d069db14265e4fcab72c25
james.olmedo <james.olmedo@infivex.com>
2020-08-31 20:45:03 +0800

    Add CORRECT_GUESS action creator and tests

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,3 +1,9 @@
+/**
+ * @function successReducer
+ * @param {array} state - Array of guessed words
+ * @param {object} action - action to be called
+ * @returns {boolean} - new success state
+ */
 export default (state, action) => {
   return null;
 };
